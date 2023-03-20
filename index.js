fc07d28d9fa9633a333af54548613460c93522d7
Moringa School <edwardgitau004@gmail.com>
2023-03-20 12:52:06 +0300

    assignment completed

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(value){
   // Write your code here!
+  //let value;
+  if( value<= 400) {
+    return 'This one is on me!'
+    } else if(value >2000 && value<= 2500) {
+    return 'I will gladly take your thirty bucks.'
+    }
+    else {
+    return 'No can do.'
+    }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  if (city==='NYC'){
+    return "Ok, sounds good.";
+}
+else {
+  return 'No go.';
+}
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tipAmount){
   // Write your code here!
+  if(tipAmount==='generous'){
+    return 'Thank you so much.';}
+else if (tipAmount=== 'not as generous'){
+  return'Thank you.';
+} else{
+  return 'Bye.';
+}
 }
